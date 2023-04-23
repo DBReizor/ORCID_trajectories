8a16ac181fd1ff5c3667f73f193c7ca09c5594cd
Donovan Blount <93835259+DBReizor@users.noreply.github.com>
2023-04-23 18:29:41 -0400

    Everything works?

--- data_processing/parse_orcid_json_files.py
+++ data_processing/parse_orcid_json_files.py
@@ -3,44 +3,54 @@
 # In[ ]:
 
 import glob
-import ujson as json
 import gzip
+import json
 import os
 
 JSON_DATA_DIR = "../../data/raw/"
 
-#this is a comment
 
 # In[ ]:
 
+#we removed all the encodes to utf-8 since we open the file in utf8 mode, also not sure where replacing tabs/ \
+# is applicable
+
 def parse_bio(bio_part):
     first_name, last_name = "None", "None"
-    #done
     if bio_part["person"]["name"]["given-names"]:
-        first_name = bio_part["person"]["names"]["given-names"]["value"].encode('utf-8').replace("\t", " ")
-    #done
+        first_name = bio_part["person"]["name"]["given-names"]["value"] #.encode('utf-8').replace("\t", " ")
     if bio_part["person"]["name"]["family-name"]:
-        last_name = bio_part["person"]["name"]["family-name"]["value"].encode('utf-8').replace("\t", " ")
-    #done
-    if bio_part["person"]["biography"]:
-        bio = bio_part["person"]["biography"]['value'].encode('utf-8').replace("\t", " ").replace("\n", " ")
+        last_name = bio_part["person"]["name"]["family-name"]["value"]  #.encode('utf-8').replace("\t", " ")
+    if bio_part["person"]["biography"]["content"]:
+        bio = bio_part["person"]["biography"]['content'] #.encode('utf-8').replace("\t", " ").replace("\n", " ")
+        #used to be biography['value'] - unsure what value is supposed to be grabbed (2 instances)
     else: bio = "None"
-    if bio_part["person"]["researcher-urls"]: #done
-        researcher_urls = [unicode(u['url']['value']).replace("\t", " ") for u in bio_part["person"]["researcher-urls"]["researcher-url"]]
+    if bio_part["person"]["researcher-urls"]: 
+        researcher_urls = [str(u['url']['value']).replace("\t", " ") for u in bio_part["person"]["researcher-urls"]["researcher-url"]]
     else: researcher_urls = "None"
-    if bio_part["person"]["addresses"]: #done
+    if bio_part["person"]["addresses"]: 
         if bio_part["person"]["addresses"]["address"]:
-            country = bio_part["person"]["addresses"]["address"]["country"]["value"].replace("\t", " ")
+            country=[]
+            countryL = bio_part["person"]["addresses"]["address"] #[0]["country"]["value"] #.replace("\t", " ")
+            for dic in countryL:
+                if not dic["country"]:
+                    continue
+                country.append(dic["country"]["value"])
+
+            #ask kenny for help, not sure if dict is always in 0th position or not
+            #need to check if multiple addresses can exist within "addresses" 
+                    
         else: country = "None"
     else: country = "None"
+
     if bio_part["person"]['keywords']: #done
-        keywords = [str(k['value'].encode('utf-8')).replace("\t", " ") for k in bio_part["person"]['keywords']['keyword']]
+        keywords = [str(k["source"]["source-name"]['value'].encode('utf-8')) for k in bio_part["person"]['keywords']['keyword']]
     else: keywords = "None"
+    
     return [first_name, last_name, str(bio), str(researcher_urls), str(country), str(keywords)]
 
-
 def parse(filename):
-    with open(filename) as data_file:
+    with open(filename, encoding="utf8") as data_file:
         data = json.load(data_file)
 
         #ID
@@ -51,6 +61,14 @@ def parse(filename):
             bio_values = parse_bio(data)
         else:
             bio_values = ["None", "None", "None", "None", "None", "None"]
+            
+    int=0
+    values=["first_name: ", "family_name: ", "biography: ", "urls: ", "country: ", "keywords: "]
+    for val in bio_values:
+        print()
+        print("parse result "+ values[int])
+        print(val)
+        int+=1
     return bio_values, id
 
 def parse_affiliations(file_name):
@@ -61,26 +79,18 @@ def parse_affiliations(file_name):
     aff = f['orcid-activities'].get('affiliations',{})
     if not aff:
         return {}
+    print()
+    print("parse_affiliations result")
     return aff
     
 
 
 # In[ ]:
 
-fils = glob.glob(os.path.join(JSON_DATA_DIR, "*"))
-with gzip.open("../../data/output/affiliations.json.gz","w") as of:
-    f = open("fail.txt","w")
-    for i,fil in enumerate(fils):
-        if i % 25000 == 0:
-            print i
-        try:
-            affils = parse_affiliations(fil)
-            if len(affils):
-                bio = parse(fil)
-                affils['bio'] = bio[0]
-                affils['id'] = bio[1]
-                of.write(json.dumps(affils).strip().encode("utf8") + "\n")
-        except:
-            f.write(fil+"\n")
-    f.close()
 
+# In[ ]:
+
+parse('0000-0002-2032-5000.json')
+
+
+# %%
